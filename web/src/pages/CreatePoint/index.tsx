87eae51265a5c669844c111068abcab906919e48
ThoNogueira <thonogueira@gmail.com>
2020-07-16 11:26:29 -0300

    Adição do front.

---
+++ web/src/pages/CreatePoint/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
+
+import './styles.css'
+import logo from '../../assets/logo.svg'
+
+const CreatPoint = () => {
+    return (
+        <div id="page-create-point">
+            <header>
+                <img src={logo} alt="Ecoleta" />
+
+                <Link to="/">
+                    <FiArrowLeft /> Voltar para home
+                </Link>
+            </header>
+        </div>
+    );
+}
+
+export default CreatPoint;
